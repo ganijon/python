f4cc8ce84ed76ce209f3d50322def343e6317cdf
Ganijon R. <ganijon.rahimov@gmail.com>
2026-03-23 13:04:12 -0500

    practive files and classes

--- python-course-greg-hogg/classes.py
+++ python-course-greg-hogg/classes.py
@@ -8,6 +8,9 @@ class Human:
         
     def __repr__(self):
         return f"Human(age={self._age}, name='{self._name}')" 
+    
+    def __str__(self):
+        return f"{self._name} is {self._age}"
         
 h = Human(15, 'BJ')
 
--- python-course-greg-hogg/files.py
+++ python-course-greg-hogg/files.py
@@ -1,34 +1,35 @@
-print('---write to a new file----')
+print('# Let\'s write to a new file')
 with open('newfile.txt', 'w') as f:
     f.write('Hi!')
 
-print('---print() to a file----')
+print('# Then print() to a file')
 with open('newfile.txt', 'w', encoding='utf-8') as f:
     print('overwrite file', file=f)
     print('use print() to a file!', file=f)
     pi = 3.14
     print(f'print a value to file: {pi}', file=f, flush=True)
     print(f'print a value to stdout: {pi}')
-    
+
+print('# Now, append some text to a file')
 name="Python"
 with open('file.txt', 'a') as f:
     f.write(f'\nHello, {name}!')
     
-print('----read all file----')    
+print('# Then, just read all of it')    
 with open('file.txt', 'r') as f:
     print(f.read())
 
-print('----read all lines----')    
+print('# Try read all lines, but print by line')    
 with open('file.txt', 'r') as f:
     for line in f.readlines():
         print(line.rstrip())
         
-print('----read line by line----')    
+print('# Try read line by line')    
 with open('file.txt', 'r') as f:
     for line in f:
         print(line.rstrip())
 
-print('----read & write line by line----')    
+print('# Finally, read from one file by line & write to another file with line numbers')    
 with open('file.txt', 'r') as f1:
     with open('copy.txt', 'r+') as f2:
         for i, line in enumerate(f1):
