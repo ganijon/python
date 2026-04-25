6619bcb67ed5a1131df3e674d4808eb461cb534c
Ganijon R. <ganijon.rahimov@gmail.com>
2026-04-25 17:59:47 -0500

    add loops.py with basic sequence and enumeration examples

---
+++ python-course-greg-hogg/loops.py
@@ -0,0 +1,11 @@
+sequence = [1,2,3,4,5]
+
+for item in sequence:
+    print(item)
+
+for i in range(10, 0, -2):
+    print(i)
+
+points = [[0, 0], [1, 1], [1, 0], [0, 1]]
+for i, point in enumerate(points):
+    print(i, point)
